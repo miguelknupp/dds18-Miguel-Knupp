8e40efd5d6097f1a0614260e9b488a7785da20c6
Miguel Knupp <miguel.barboza@aluno.senai.br>
2024-08-21 16:55:51 -0300

    aula boa

---
+++ exercicios/exercicio 2/js.js
@@ -0,0 +1,9 @@
+var numero1 = prompt("digite um numero")
+
+var numero2 = prompt("digite outro numero")
+
+var soma = Number(numero1) + Number(numero2)
+
+alert("seu numero é " + soma)
+
+console.log("seu numero é " + soma)
---
+++ pasta de aulas/aula 1 javascript/script.js
@@ -0,0 +1,102 @@
+// alert("em caminho de paca, tatu caminha dentro?")
+
+// console.log("OLHA CACHORRO")
+
+// var chupisco = "chuvisco*"
+
+// var numero = 1989
+
+// var theBest = true
+
+// var nulo = null
+
+// console.log(chupisco,numero,theBest,nulo)
+
+// operadores 
+
+// aritimeticos: +-/*%
+
+var a = 10
+var b = 3
+
+console.log(a+b,a*b,a/b,a-b,a%b)
+console.log("japa",(a+b))
+
+// &&, ||, !
+
+var verdade = true
+var mentira = false
+
+console.log(verdade && mentira) // ambas verdadeiras ?
+console.log(verdade || mentira) // verifica se uma das duas é verdadeira, se tiver ignora a falsa
+console.log(verdade && !mentira) // o ! nega a afirmação
+
+// relacionais: >,<,>=,<=,==,!=,===,!==
+
+
+/*
+if(){
+
+}
+else if(){
+
+}
+else{
+
+}
+
+
+
+
+switch(){
+    case ahaha:
+        console.log();
+        break
+        
+}
+        */
+
+
+
+/*
+// loop
+
+// for
+
+for(var i = 0; i < 25; i++){
+    console.log("a", i);
+    
+}
+    console.log("feeling loney");
+    
+
+// while
+
+    var it = 1
+    while (it < 10){
+        console.log("Estou aqui")
+        it++
+        
+    }
+*/
+
+// função
+/*
+function alfredo(){
+    console.log("estou funçãonando");
+    
+}
+
+function alfredo2(){
+    console.log("estou funçãonando só que dois");
+    
+}
+
+alfredo2()
+*/
+
+//usurio interage via prompt
+
+var resposta = prompt()
+
+console.log(resposta);
