1dc217e9f0b2a52e46489d7f6df282762dbe1bbd
Miguel Knupp <miguel.barboza@aluno.senai.br>
2024-08-23 16:13:31 -0300

    salsa

--- pasta de aulas/aula 10/kaksksak.js
+++ pasta de aulas/aula 10/kaksksak.js
@@ -16,27 +16,25 @@ function media (n1,n2){
     return((n1+n2)/ 2)
 }
 
-function resultado(){
-    var mediaAtual = media(n1,n2)
-    
-    if(mediaAtual < 5){
-        var lascou = "reprovado"
-    }
-    else if(mediaAtual > 5 && mediaAtual < 7){
-        var lascou = " recuperação"
-    }else{
-        var lascou = " aprovado"
-    }
-}
 function resultadoGeral(){
 
     for(var index in nome){
         var n1 = nota1[index]
         var n2 = nota2[index]
         var mediaAtual = media(n1,n2)
+        var lascou = null
+        if(mediaAtual < 5){
+            var lascou = "reprovado"
+        }
+        else if(mediaAtual > 5 && mediaAtual < 7){
+            var lascou = " de recuperação"
+        }else{
+            var lascou = " aprovado"
+        }
+        console.log("o aluno", nome[index], "teve media", mediaAtual, " e esta", lascou);
     }
 
-    console.log("o aluno", nome[index], "teve media", mediaAtual, " e esta", lascou);
+    
     
 
 }
