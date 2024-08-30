9bab8fa3594028293a6a37d120f8a78d39e304ee
Miguel Knupp <miguel.barboza@aluno.senai.br>
2024-08-30 15:17:15 -0300

    h

---
+++ pasta de aulas/aula12/script.js
@@ -0,0 +1,33 @@
+
+//objeto window = janela do navegador
+/*
+window.console.log("testado")
+window.alert("cu")
+window.prompt("acordei")
+
+
+// objeto document = codigo htl
+console.log(document.head)
+console.log(document.body)
+
+
+// ascessando elemetos por class e id
+var titulo = document.getElementsByTagName("h1")
+var paragrafos = document.getElementsByClassName("para")
+var para3 = document.getElementById("v3")
+
+paragrafos[0].innerText = "tabua"
+
+para3.innerHTML = "<h3> novo texto aqui </h3>"
+
+var p1 = document.getElementsByTagName("p")[0]
+p1.style.color = "red"
+
+para3.style.backgroundColor = "green"
+
+*/
+
+function cliquei(){
+    console.log("cliquei de novo")
+}
+
